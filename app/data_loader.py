85de92a079c1c455aa8a5cc021e08b79d476ed01
kecichilala <k.n.franciscochilala@student.tue.nl>
2023-09-29 17:49:46 +0200

    create a function that reads the csv file

--- app/data_loader.py
+++ app/data_loader.py
@@ -11,4 +11,19 @@ from typing import List, Dict
 
 
 # // BEGIN_TODO [task_2] Loading and preparing the dataset
+path = "/Users/kecichilala/PycharmProjects/h11-assignment-2-template/assets/beer_db_v4.csv"
+def read_dataset(path: str) -> list[dict]:
+    """
+    this function opens the file name and reads the csv file
+    and returns a list of dictionary's.
+
+    :parameter: this is a string of the path of the file
+    :returns: it returns a list of dictionaries
+
+    """
+
+    with open(path, 'r') as file:
+        data = csv.reader(file)
+        for row in data:
+            print(row)
 # // END_TODO [task_2]
